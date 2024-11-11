b8b9aba505f9b99b4a7bbcc52783fb545b4556d1
rl <robertliu022@gmail.com>
2024-11-11 04:39:17 -0600

    fix bd

--- sparsity.py
+++ sparsity.py
@@ -17,6 +17,7 @@ def sparsify_threshold_based(model, sparsity_level):
     layer_weights = defaultdict(list)
     for name, param in model.named_parameters():
         if "weight" in name:  # Filter to include only weight parameters
+            if len(name.split(".") < 4): continue
             layer = name.split(".")[3]
             layer_weights[layer].append((layer, param))
     
